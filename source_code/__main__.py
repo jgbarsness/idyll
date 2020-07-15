06e52902b0a10b279f67f0b444efc7265b0038e1
Joseph Barsness <Joseph@MacBook-Pro.local>
2020-07-15 01:28:01 -0700

    file path logic

--- source_code/__main__.py
+++ source_code/__main__.py
@@ -19,8 +19,6 @@ def main(sys_arguement=None, title=None):
             print('\n' + 'collection: ' + c.PURPLE + os.path.abspath(c.JOURNAL_TITLE) + c.END)
             if os.path.exists(c.BACKUP_TITLE):
                 print('backup: ' + c.PURPLE + os.path.abspath(c.BACKUP_TITLE) + c.END)
-        else:
-            print('\n' + c.PURPLE + 'in an unused directory' + c.END)
         print(c.HEADER + c.HELP)
         return
     # check files
