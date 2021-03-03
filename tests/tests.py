15473b7021bfdaaca29414e45d9d72b8a72a4cad
Joseph Barsness <64283969+jgbarsness@users.noreply.github.com>
2021-03-03 14:18:51 -0800

    test: test removed from gitignore

---
+++ tests/tests.py
@@ -0,0 +1,15 @@
+import unittest
+
+class TestRoutes(unittest.TestCase):
+
+    def test_full(self):
+        pass
+
+    def test_tag(self):
+        pass
+
+    def test_markers(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
