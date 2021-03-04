26ae9a5df42c74493520e44bf85dbcc41c0ece65
Joseph Barsness <64283969+jgbarsness@users.noreply.github.com>
2021-03-03 22:01:11 -0800

    tests: tmp remove imports

--- tests/tests.py
+++ tests/tests.py
@@ -1,10 +1,4 @@
 import unittest
-from peck.full_entry import FullEntry
-from peck.title_entry import TitleEntry
-from peck.first_entry import FirstEntry
-from peck.second_entry import SecondEntry
-from peck.tag_entry import TagEntry
-from peck.collection import Collection
 
 class TestRoutes(unittest.TestCase):
 
