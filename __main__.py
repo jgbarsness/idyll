f86ddbda39dc068501907b29ed40b3bd83350d64
Joseph Barsness <64283969+jgbarsness@users.noreply.github.com>
2021-03-04 22:19:40 -0800

    doc: __main__

--- __main__.py
+++ __main__.py
@@ -2,6 +2,12 @@ from peck.peck import peck
 import sys
 
 
-def main():
-    if __name__ == '__main__':
-        peck.peck.driver()
+'''
+__main__ is pulled out into the parent directory
+to allow for consistent import format in peck.
+run this script as an entry point during development,
+put note that peck.driver() is called directly when
+peck is installed from pip
+'''
+if __name__ == '__main__':
+    peck.peck.driver()
