8eaaffe410e268d5b0742f5424c7919e46f40b60
Joseph Barsness <64283969+jgbarsness@users.noreply.github.com>
2021-03-04 22:15:08 -0800

    dir: __main__ -> parent dir for development

---
+++ __main__.py
@@ -0,0 +1,7 @@
+from peck.peck import peck
+import sys
+
+
+def main():
+    if __name__ == '__main__':
+        peck.peck.driver()
--- peck/__main__.py
+++
@@ -1,6 +0,0 @@
-import peck
-import sys
-
-
-if __name__ == '__main__':
-    peck.driver()
